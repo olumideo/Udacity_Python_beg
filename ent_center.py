73ff7a98f322ee5a3261b8b15ed404c2a95724af
mideguan <mide.oluyi@outlook.com>
2017-08-07 03:55:30 +0200

    Create ent_center.py
    Calls the Movie class; making instances for the movie website

---
+++ ent_center.py
@@ -0,0 +1,7 @@
+import media
+
+toy_story = media.Movie("Toy Story", " A story of a boy ..","https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg","https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
+
+toy_story.show_trailer()
